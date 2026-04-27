f6878ce5c287ea7249b076bf378307b443592d83
Payrt <payrtovv@gmail.com>
2026-04-27 12:43:46 -0500

    Carpetas templates y static para el frontend

--- Liszt/users/urls.py
+++ Liszt/users/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index)]
+urlpatterns = [path("", views.index),
+               path('login/', views.login_view, name='login'),
+]
--- Liszt/users/views.py
+++ Liszt/users/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Persona
 
 
 # Create your views here.
 def index(request):
     return HttpResponse("prueba")
+
+
+def login_view(request):
+    if request.method == 'POST':
+        correo = request.POST['correo']
+        contrasenia = request.POST['contrasenia']
+        try:
+            persona = Persona.objects.get(correo=correo, contrasenia=contrasenia)
+            request.session['usuario_id'] = persona.idpersona
+            return redirect('home')
+        except Persona.DoesNotExist:
+            return render(request, 'users/login.html', {'error': 'Credenciales incorrectas'})
+    return render(request, 'users/login.html')
